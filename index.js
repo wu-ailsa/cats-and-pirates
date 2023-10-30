80b45ba823932829b6416fd6f317cdd1b291989e
wu-ailsa <ailsawunyc@gmail.com>
2023-10-30 16:15:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//-----------------------------------------------------
+//  "Don't panic" - Hitchhiker's Guide to the Galaxy
+//-----------------------------------------------------
+
+// Cat Object
+// Instances 
+
+// Pirate Object
+// Instances 
